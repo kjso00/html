bd4501c84b442a41adf746df3e841e659e3a4f94
kjso00 <166350681+kjso00@users.noreply.github.com>
2024-05-24 17:08:58 +0900

    sfd
    11q

---
+++ 02_core/07_object-constructor-function/student.js
@@ -0,0 +1,12 @@
+class student{
+    constructor(name){
+        this.name = name;
+        this.age = age;
+    }
+
+    getinfo() {
+        return `${this.name}의 나이는 ${this.age}입니다.`
+    } 
+}
+
+export default Student;
